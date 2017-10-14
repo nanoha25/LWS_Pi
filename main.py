3325c76288070273729b7e6f02cd7db0c8f4ce5d
nanoha25 <nanohachina@takamachi.onmicrosoft.com>
2017-10-14 23:47:10 +1100

    First  combined version

---
+++ main.py
@@ -0,0 +1,31 @@
+from sense_hat import SenseHat
+import time
+import httplib, urllib
+
+while True:
+    sense_nanoha = SenseHat()
+
+    pressure = sense_nanoha.get_pressure()
+    temp = sense_nanoha.get_temperature()
+    humidity = sense_nanoha.get_humidity()
+
+    params = urllib.urlencode({'field1': 12, 'field2': 11, 'key': '9B1ZCDNMTLRHRXRP'})
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    conn = httplib.HTTPConnection("api.thingspeak.com:80")
+    conn.request("POST", "/update", params, headers)
+    response = conn.getresponse()
+    conn.close()
+    params = urllib.urlencode({'field1': 3, 'field2': 2, 'key': '9B1ZCDNMTLRHRXRP'})
+    conn = httplib.HTTPConnection("api.thingspeak.com:80")
+    conn.request("POST", "/update", params, headers)
+    response = conn.getresponse()
+    conn.close()
+
+    time.sleep(30)
+
+    # put = input("Type 1 to terminate")
+    # brk = int(put)
+    # if brk==1:
+    #    break
+
+
