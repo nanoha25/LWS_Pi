d5d9fe2f051f2f0e1462097748b1a36389c14b65
Leo <yilutongzai@gmail.com>
2017-10-30 05:31:30 +1100

    co-helper user receive vibrate when the other user fell

--- Accelerator4/MainView.swift
+++ Accelerator4/MainView.swift
@@ -11,13 +11,19 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 import FirebaseAuth
-
+import AudioToolbox
 import CoreMotion
 
 class MainView: UIViewController {
+    @IBOutlet weak var notificationField: UILabel!
+     var Offerhelpname_pub:String!
+    var counter:Int = 0
+    var timer : Timer?
+    var notification:String!
     var motionManager = CMMotionManager()
     var motionManager_Gyro = CMMotionManager()
     @IBOutlet weak var CancelResult: UIButton!
+    @IBOutlet weak var CancelMonitor: UIButton!
     @IBOutlet weak var x: UILabel!
     @IBOutlet weak var y: UILabel!
     @IBOutlet weak var z: UILabel!
@@ -25,7 +31,9 @@ class MainView: UIViewController {
     @IBOutlet weak var alarm: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.notificationField.text = self.notification
         self.CancelResult.isHidden = true
+        self.CancelMonitor.isHidden = true
         motionManager.accelerometerUpdateInterval = 0.1
         motionManager_Gyro.gyroUpdateInterval = 0.1
         motionManager.startAccelerometerUpdates(to: OperationQueue.current!){(data,error)in
@@ -71,7 +79,115 @@ class MainView: UIViewController {
     @IBAction func cancelResult(sender: AnyObject?){
         self.CancelResult.isHidden = true
         self.alarm.text = nil
+        let userUid = Auth.auth().currentUser?.uid
+        let setLocation =  Database.database().reference().child("users").child(userUid!).child("Fall")
+        setLocation.setValue("False")
        
+    }
+    @IBAction func MonitorBegin(sender: AnyObject?){
+        let ref = Database.database().reference().child("users")
+        let userUid = Auth.auth().currentUser?.uid
+        var Offerhelpname:String!
+        self.CancelMonitor.isHidden = false
+        ref.child(userUid!).observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            print(snapshot)
+            //print(snapshot.hasChild("username"))
+            print("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+            if snapshot.exists()
+            {
+                
+                print("user exist")
+                
+                    let dict = snapshot.value as! [String: String] // the value is a dict
+                      Offerhelpname = dict["UsernameOfferHelp"]
+                self.Offerhelpname_pub = Offerhelpname
+                print("Offer help to \(Offerhelpname!)")
+                 print("the offerhelp public is \(self.Offerhelpname_pub)")
+                print("the name of offfhelpname \(Offerhelpname)")
+                if Offerhelpname != nil
+                {
+                    let ref = Database.database().reference().child("users")
+                    
+                    //            refHandle = postRef.observe(DataEventType.value, with: { (snapshot) in
+                    //                let postDict = snapshot.value as? [String : AnyObject] ?? [:]
+                    //                // ...
+                    //            })
+                    print("the position before query")
+                    ref.queryOrdered(byChild:"Username").queryEqual(toValue: Offerhelpname).observe(DataEventType.value, with: { (snapshot) in
+                        
+                        print(snapshot)
+                        //print(snapshot.hasChild("username"))
+                        print("get snapshot of the UsernameOfferHelp")
+                        if snapshot.exists()
+                        {
+                            var fall:String!
+                            print("UsernameOfferHelp exist")
+                            for child in (snapshot.children)
+                            {
+                                let snap = child as! DataSnapshot
+                                let dict = snap.value as! [String: String] // the value is a dict
+                                
+                                fall = dict["Fall"]
+                            }
+//                            let  myAlert_succ = UIAlertController(title:"If the elder fall", message: fall, preferredStyle:.alert)
+//
+//                            self.present(myAlert_succ,animated: true,completion: nil)
+                            if  fall! == "True"
+                            {
+                                // alarm
+                                AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                                print("The elder falls")
+                                self.notification = "The elder fell!!!"
+                                self.notificationField.text = self.notification
+//                                self.counter = 0
+//                                self.timer = Timer.scheduledTimer(timeInterval: 0.6, target: self, selector: Selector(("vibratePhone")), userInfo: nil, repeats: true)
+//                                self.vibratePhone()
+                                
+                            }
+                            else
+                            {
+                                print("The elder is well")
+                            }
+                        }
+                        else
+                        {
+                            
+                            print("snapshot of fall not exist!")
+                        }
+                    }
+                    )
+                    
+                }
+                else
+                {
+                    self.notification = "The usrname offer help to doesn't exist"
+                    print("*********************\(self.notification)")
+                    print("the offerhelp public is \(self.Offerhelpname_pub)")
+                }
+            }
+            else
+            {
+                print("error when getting the username of offerHelpTo")
+            }
+        })
+        
+        
+    }
+//    func vibratePhone() {
+//        self.counter+=1
+//        switch self.counter {
+//        case 1, 2:
+//            AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+//        default:
+//            self.timer?.invalidate()
+//        }
+//    }
+    
+  
+    @IBAction func cancelMonitor(sender: AnyObject?){
+        self.CancelMonitor.isHidden = true
+        // caneal monitor event
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
