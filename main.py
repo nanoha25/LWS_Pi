86f2698bc53ff82e37f83a76462bcb4bdb52e3b2
nanoha25 <nanohachina@takamachi.onmicrosoft.com>
2017-10-29 22:52:28 +1100

    Adds comments for documentation purpose
    And also enable "while True" so that the code can run normally.

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import httplib, urllib
 
 
 def weather_station():
-
+    # setup board and sensors.
     sense_nanoha = SenseHat()
 
     pressure = sense_nanoha.get_pressure()
     temp_ori = sense_nanoha.get_temperature()
     humidity = sense_nanoha.get_humidity()
-
+    #add a modifier to let the final result closer to the actual value.
     ftr = 1.39
     temp = temp_ori / ftr
-
+    #send data to thingspeak.
     params = urllib.urlencode(
         {'field1': temp, 'field2': humidity, 'field3': pressure, 'key': '9B1ZCDNMTLRHRXRP'})
     headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
@@ -22,11 +22,13 @@ def weather_station():
     conn.request("POST", "/update", params, headers)
     response = conn.getresponse()
     conn.close()
-
+    #add a timer as per requirement from thingspeak.
     time.sleep(30)
 
-    print('Sent.\n')
+    print('Sent.\n')  #provide terminal output indicating data is sent.
     return 0
 
-#while True:
-    weather_station()
+
+
+while True:
+    weather_station() #run the function.
