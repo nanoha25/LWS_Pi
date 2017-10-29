4484f01ad66f0f23f516baef5bf4ab31be403f33
nanoha25 <nanohachina@takamachi.onmicrosoft.com>
2017-10-29 17:32:41 +1100

    Adds on-screen output to test the feature.
    This is not the right way to test but whatever. Code is broken for sure.

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ class MainFuc:
             pressure = sense_nanoha.get_pressure()
             temp_ori = sense_nanoha.get_temperature()
             humidity = sense_nanoha.get_humidity()
-
+            print('Got data!\n')
             return temp_ori, pressure, humidity
 
         def modifier(self, temp_ori):
             ftr = 1.39
             temp = temp_ori/ftr
-
+            print('Data modified!\n')
             return temp
 
         def send_data(self, temp, pressure, humidity):
@@ -28,10 +28,12 @@ class MainFuc:
             conn.request("POST", "/update", params, headers)
             response = conn.getresponse()
             conn.close()
+            print('Data sent!\n')
             return response
 
         def timer(self):
             time.sleep(30)
+            print('Woke up!\n')
             return 0
 
             # put = input("Type 1 to terminate")
